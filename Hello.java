ed59304cc0864b109dfaed83f881d9cdeb4f533e
Harvey <harvey0379@163.com>
2021-05-23 21:38:53 +0800

    main

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+ * doc comment
+ */
+class Demo {
+    // main
+    public static void main(String[] args) {
+        /*
+            multi line comment
+         */
+        System.out.println("Hello World");
+    }
+}
