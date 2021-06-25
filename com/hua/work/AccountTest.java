11d77ac61fbf5a0aefc01f69d1e6e8ae0e4718cd
Harvey <harvey0379@163.com>
2021-06-25 14:37:55 +0800

    chain opetate

---
+++ com/hua/work/AccountTest.java
@@ -0,0 +1,12 @@
+package com.hua.work;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Account a = new Account();
+
+
+        a.setId(1).setName("Hua");
+
+        System.out.println(a);
+    }
+}
