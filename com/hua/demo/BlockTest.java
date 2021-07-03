041a0a41fe536f705396acb7af9bebba4b2d8184
Harvey <harvey0379@163.com>
2021-07-03 21:17:41 +0800

    block

--- com/hua/demo/BlockTest.java
+++ com/hua/demo/BlockTest.java
@@ -1,13 +1,42 @@
+package com.hua.demo;
+
+import org.junit.Test;
+
 public class BlockTest {
 
-  @Test
-  public void test() {
-    System.out.println(1);
-  }
+    @Test
+    public void test() {
+        Student s1 = new Student();
+        System.out.println("-----------------------------");
+        Student s2 = new Student();
+    }
 }
 
 
-class Person {
-  String name;
-  int age;
+class Man {
+    static {
+        System.out.println("Man's static block");
+    }
+
+    {
+        System.out.println("Man's block");
+    }
+
+    Man() {
+        System.out.println("Man's constructor");
+    }
+}
+
+class Student extends Man {
+    static {
+        System.out.println("Student's static block");
+    }
+
+    {
+        System.out.println("Student's block");
+    }
+
+    Student() {
+        System.out.println("Student's constructor");
+    }
 }
