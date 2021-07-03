c273a683c5867015a1110280a4f1e433088d645e
Harvey <harvey0379@163.com>
2021-07-03 14:49:50 +0800

    jul 13

---
+++ com/hua/family/Test.java
@@ -0,0 +1,18 @@
+package com.hua.family;
+
+public class Test {
+    public static void main(String[] args) {
+        Martin m = new Martin();
+
+        System.out.println(m.lastname);
+        m.introduce();
+        m.introduce("Martin Hua");
+        System.out.println(m.getAge());
+        m.play();
+
+        Harvey m1 = new Martin();
+        System.out.println(m1.lastname);
+        System.out.println(m1.getAge());
+        m1.play();
+    }
+}
