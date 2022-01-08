0bce8bab8d50e7bf6e6da218b34c99a87cb313c3
huaweiwei <harvey0379@163.com>
2022-01-09 00:53:14 +0800

    Math.abs

---
+++ src/main/java/com/hua/demo/NumberDemo.java
@@ -0,0 +1,8 @@
+package com.hua.demo;
+
+public class NumberDemo {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+        System.out.println(Math.absExact(Integer.MIN_VALUE));
+    }
+}
