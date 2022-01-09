e42de4bf313124c2b770c05c934656c6320c6896
huaweiwei <harvey0379@163.com>
2022-01-09 22:37:09 +0800

    jdk 15 required

--- src/main/java/com/hua/demo/NumberDemo.java
+++ src/main/java/com/hua/demo/NumberDemo.java
@@ -3,6 +3,7 @@ package com.hua.demo;
 public class NumberDemo {
     public static void main(String[] args) {
         System.out.println(Math.abs(Integer.MIN_VALUE));
-        System.out.println(Math.absExact(Integer.MIN_VALUE));
+        // JDK 15
+        // System.out.println(Math.absExact(Integer.MIN_VALUE));
     }
 }
