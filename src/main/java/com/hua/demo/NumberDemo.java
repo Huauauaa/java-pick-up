382c2582bd17e418ee13fef9eb656bd432c87d10
huaweiwei <harvey0379@163.com>
2022-01-10 08:07:42 +0800

    Revert "jdk 15 required"
    This reverts commit e42de4bf313124c2b770c05c934656c6320c6896.

--- src/main/java/com/hua/demo/NumberDemo.java
+++ src/main/java/com/hua/demo/NumberDemo.java
@@ -3,7 +3,6 @@ package com.hua.demo;
 public class NumberDemo {
     public static void main(String[] args) {
         System.out.println(Math.abs(Integer.MIN_VALUE));
-        // JDK 15
-        // System.out.println(Math.absExact(Integer.MIN_VALUE));
+        System.out.println(Math.absExact(Integer.MIN_VALUE));
     }
 }
